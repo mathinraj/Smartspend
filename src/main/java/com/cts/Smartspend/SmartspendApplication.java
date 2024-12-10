52841f9a800d1a1918757ba91dfb04fd949f0ad1
Mathinraj <mathinraj7@gmail.com>
2024-12-10 21:28:34 +0530

    Created the basic spring files

---
+++ src/main/java/com/cts/Smartspend/SmartspendApplication.java
@@ -0,0 +1,13 @@
+package com.cts.Smartspend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartspendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartspendApplication.class, args);
+	}
+
+}
